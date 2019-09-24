f50d6140f03449d591c11dc00754f23f25a1315d
kevinbacas <bacas.kevin@hotmail.fr>
2019-09-24 17:11:42 +0200

    Remove dep usage and unused imports

--- Handler/Rest/Ping.go
+++ Handler/Rest/Ping.go
@@ -1,14 +1,8 @@
 package Rest
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-
-	"github.com/Augora/Augora-GraphQL/Models"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
