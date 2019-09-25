3034e417060d7d32295eb16368058d4a8a8804b7
kevinbacas <bacas.kevin@hotmail.fr>
2019-09-25 20:35:14 +0200

    Add endpoint for single deputy

---
+++ Handler/Rest/Deputy.go
@@ -0,0 +1,22 @@
+package Rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/Augora/Augora-GraphQL/Models"
+	"github.com/Augora/Augora-GraphQL/Utils"
+)
+
+func DeputiesHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Cache-Control", "s-maxage=3600")
+	db := Utils.GetDataBaseConnection()
+
+	var depute Models.Depute
+	slug := r.URL.Query()["slug"]
+	db.Preload("SitesWeb").Preload("Emails").Preload("Adresses").Preload("Collaborateurs").Where(&Models.Depute{Slug: slug}).Find(&depute)
+	res, _ := json.Marshal(depute)
+	fmt.Fprintf(w, string(res))
+}
