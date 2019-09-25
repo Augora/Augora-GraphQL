fd78923a062f253b6e0a3d3ab4587b450c68ce4f
kevinbacas <bacas.kevin@hotmail.fr>
2019-09-25 20:56:58 +0200

    Added error management

--- Handler/Rest/Deputy.go
+++ Handler/Rest/Deputy.go
@@ -16,7 +16,27 @@ func DeputyHandler(w http.ResponseWriter, r *http.Request) {
 
 	var depute Models.Depute
 	slug := r.URL.Query()["slug"][0]
-	db.Preload("SitesWeb").Preload("Emails").Preload("Adresses").Preload("Collaborateurs").Where(&Models.Depute{Slug: slug}).Find(&depute)
-	res, _ := json.Marshal(depute)
-	fmt.Fprintf(w, string(res))
+	queryResult := db.Preload("SitesWeb").Preload("Emails").Preload("Adresses").Preload("Collaborateurs").Where(&Models.Depute{Slug: slug}).Find(&depute)
+	errors := queryResult.GetErrors()
+	var count int
+	queryResult.Count(&count)
+	if errors != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res, _ := json.Marshal(errors)
+		fmt.Fprintf(w, string(res))
+	} else {
+		if count > 0 {
+			w.WriteHeader(http.StatusOK)
+			res, _ := json.Marshal(depute)
+			fmt.Fprintf(w, string(res))
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			var tmp struct {
+				error string `json:"error"`
+			}
+			tmp.error = "This deputy does not exists."
+			res, _ := json.Marshal(tmp)
+			fmt.Fprintf(w, string(res))
+		}
+	}
 }
